a97450414178e767b692602c087b80ca5fb57998
Danik499 <danikshcherban499@gmail.com>
2020-12-23 18:16:10 +0200

    init

---
+++ Assets/TurnOnOff.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnOff : MonoBehaviour
+{
+    public ParticleSystem ps;
+    void Start()
+    {
+        ps.enableEmission = false;
+        StartCoroutine("OffOn");
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator OffOn ()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(3);
+            ps.enableEmission = true;
+            yield return new WaitForSeconds(0.2f);
+            ps.enableEmission = false;
+        }
+    }
+}
